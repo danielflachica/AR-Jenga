ce806120348c00e76d796cdf73794dd8d3eddf68
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2019-03-15 13:51:16 +0800

    Fixed markerless application

---
+++ Assets/Vuforia/Scripts/BlockInteraction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockInteraction : MonoBehaviour
+{
+    public Camera arCamera;
+    public float minimumDistance  = 100;
+    public GameObject go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starts the arcamera script");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("updates the arcamera script");
+        if (Input.mousePresent == true)
+        {
+            // Touches performed on screen
+            Ray ray;
+            RaycastHit hit;
+            //Debug.Log("2");
+
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Mouse is clicked");
+                ray = arCamera.ScreenPointToRay(Input.mousePosition);
+                hit = new RaycastHit();
+                //Debug.Log("33");
+                if (Physics.Raycast(ray, out hit))
+                {
+
+                    if (hit.collider.gameObject.tag == "jengaBlock")
+                    {
+                        Debug.Log("Hits jenga block");
+                        go = hit.transform.gameObject;
+                        Debug.Log("Touch Detected on : " + go.name);
+                        
+                    }
+
+                }
+            }
+        }
+
+        /*RaycastHit hit;
+        Touch touch = Input.GetTouch(0);
+        Ray pointToBlock = new Ray(arCamera.transform.position, touch.position);
+
+        if(Physics.Raycast(pointToBlock, out hit, minimumDistance))
+        {
+            if(hit.collider.tag == "block")
+            {
+                Debug.Log("Hit Something");
+            }
+        }*/
+    }
+}
--- Assets/Vuforia/Scripts/GameManager.cs
+++ Assets/Vuforia/Scripts/GameManager.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public Camera arCamera;
+    public GameObject tower;
+    public GameObject plane;
+    public GameObject planeFinder;
+    private bool isPlaced = false;
+    private bool isKinematic = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray;
+        RaycastHit hit;
+
+        if (planeFinder.activeSelf == false && isPlaced == false)
+        {
+            tower.active = true;
+            Debug.Log("Tower has been placed");
+
+            int piecesCount = tower.transform.GetChildCount();
+
+            if (isKinematic)
+            {
+                unKinematic(piecesCount);
+                isKinematic = false;
+            }
+
+            isPlaced = true;
+        }
+
+
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Mouse is clicked");
+            ray = arCamera.ScreenPointToRay(Input.mousePosition);
+            hit = new RaycastHit();
+            //Debug.Log("33");
+            if (Physics.Raycast(ray, out hit, 50))
+            {
+                if (hit.collider.gameObject.tag == "jengaBlock")
+                {
+                    Debug.Log("Hits jenga block");
+                    tower = hit.transform.gameObject;
+                    Debug.Log("Touch Detected on : " + tower.name);
+
+                }
+
+            }
+        }
+
+
     }
+
+    public void unKinematic(int pieces)
+    {
+        Debug.Log("Count of Jenga Pieces: "+ pieces);
+        GameObject piece;
+
+        for (int i = 0; i < pieces; i++)
+        {
+            piece = tower.transform.GetChild(i).gameObject;
+            Rigidbody rb = piece.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+        }
+    }
+
+    
 }
