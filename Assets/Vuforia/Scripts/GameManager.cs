526745ad69255cc9fcfc80fed54c6af7fd2871cd
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2019-03-20 09:21:24 +0800

    Somewhat connected the camera to jenga block
    when the block is selected, the jenga block is tethered to the ar camera

--- Assets/Vuforia/Scripts/GameManager.cs
+++ Assets/Vuforia/Scripts/GameManager.cs
@@ -13,7 +13,19 @@ public class GameManager : MonoBehaviour
     public GameObject canvas;
     private bool isPlaced = false;
     private bool isKinematic = true;
+    private bool isTethered = false;
     private BlockInteraction interactor;
+    private Vector3 offset;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
+    // Movement speed in units/sec.
+    public float speed = 1.0F;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +36,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            
+
+        if (isTethered)
+        {
+            Debug.Log("Tethered block!");
+
+            // Distance moved = time * speed.
+            float distCovered = (Time.time - startTime) * speed;
+
+            // Fraction of journey completed = current distance divided by total distance.
+            float fracJourney = distCovered / journeyLength;
+
+            offset = arCamera.transform.localPosition - block.transform.localPosition;
+
+            // Set our position as a fraction of the distance between the markers.
+            transform.position = Vector3.Lerp(arCamera.transform.position, block.transform.position + offset, fracJourney);
+
+            /*Debug.Log("camera position: " + arCamera.transform.localPosition);
+            offset = arCamera.transform.localPosition - block.transform.localPosition;
+            Debug.Log("block old position: "+ block.transform.localPosition);
+            block.transform.localPosition = arCamera.transform.localPosition + offset;
+            Debug.Log("block new position: " + block.transform.localPosition);*/
+        }
 
         // if player touches the screen
         if (Input.GetMouseButton(0))
@@ -41,13 +74,22 @@ public class GameManager : MonoBehaviour
             {
                 if (hit.collider.gameObject.tag == "jengaBlock")
                 {
+
                     Debug.Log("Hits jenga block");
                     block = hit.transform.gameObject;
+                    block.GetComponent<Rigidbody>().useGravity = false;
+                    block.GetComponent<Rigidbody>().isKinematic = true;
                     Debug.Log("Touch Detected on block: " + block.name);
 
                     interactor.highlightObject(block);
-
+                    isTethered = true;
                     enableCanvas();
+
+                    // Keep a note of the time the movement started.
+                    startTime = Time.time;
+
+                    // Calculate the journey length.
+                    journeyLength = Vector3.Distance(arCamera.transform.position, block.transform.position);
                 }
 
             }
